a2d7c58e47dd83079471cb12ef605ec83afb26db
Gabriel Nogueira <gabrielscnogueira@gmail.com>
2023-12-14 19:18:33 -0300

    Adiciona geracao de codigo para constante

--- src/codegen.c
+++ src/codegen.c
@@ -2,17 +2,41 @@
 #include "ast.h"
 #include "emit.h"
 #include "globals.h"
+#include "tiny.tab.h"
+
+
+void genExpression(TreeNode * node){
+    switch (node->kind.exp){
+    case(ConstK):
+        emitRM("LDC", ac,node->attr.val,0);
+    defatult:
+        break;
+    }
+}
+
+void genStatement(TreeNode * node){
+    switch (node->kind.stmt){
+    case WriteK:
+
+        genExpression(node->child[0]);
+
+        emitRO("OUT", ac, 0, 0);
+        break;
+    defatult:
+        break;
+    }
+}
 
 
 void genCode(TreeNode * node){
-/*     if (node != NULL) { */
-/*         if (node -> nodekind == StmtK) { */
-/*             genStatement(node); */
-/*         } else if (node -> nodekind == ExpK){ */
-/*             genExpression(node); */
-/*         } */
-/*         genCode(node->sibling); */
-/*     } */
+    if (node != NULL) {
+        if (node -> nodekind == StmtK) {
+            genStatement(node);
+        } else if (node -> nodekind == ExpK){
+            genExpression(node);
+        }
+        genCode(node->sibling);
+    }
 }
 
 void generateCode(TreeNode * tree){
@@ -21,7 +45,8 @@ void generateCode(TreeNode * tree){
     emitRM("LD",mp_reg,0,ac);
     emitRM("ST",ac,0,ac);
 
-    /* genCode(tree); */
+    genCode(tree);
 
     emitRO("HALT",0,0,0);
 }
+
